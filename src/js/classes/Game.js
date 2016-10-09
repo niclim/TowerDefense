eff8ba27fb48298cd1e1ad45c75b2b5574b1c6fb
Nicholas Lim <nicholaslim49@gmail.com>
2016-10-09 14:43:38 -0700

    -A

--- src/js/classes/Game.js
+++ src/js/classes/Game.js
@@ -2,6 +2,14 @@ var GameEngine = function() {
   this.userGold = 10;
   this.level = 1;
   this.userLives = 30;
+
+
+  // Goes through and disables towers that can't be used yet
+  // Tower placed method
+  // game has some sort of memory remembering where the towers are placed
+  // pubsub implementation
+  // each game cycle sends update to each monster and tower to run on a game cycle
+  // the constructor makes it that they subscribe to it - when die they unsubscribe
 }
 
 module.exports = GameEngine;
--- src/js/interface.js
+++ src/js/interface.js
@@ -1,6 +1,7 @@
 // Initiate all the components
 var Tower = require("./classes/Tower.js");
 var GameEngine = require("./classes/Game.js");
+var game;
 // On start, create a game element - these event listeners will be destroyed when the container is overriden
 document.getElementById("start-btn").addEventListener("click", function() {
   // Hides the modal lightbox
@@ -8,6 +9,7 @@ document.getElementById("start-btn").addEventListener("click", function() {
   document.getElementsByClassName("modal-background")[0].style.display = "none";
 
   // Start the game engine
+  game = new GameEngine;
 });
 
 document.getElementById("information-btn").addEventListener("click", function() {
