7a7fea712973baa0e100822c3cb71bd213c82348
Nicholas Lim <nicholaslim49@gmail.com>
2016-10-09 21:44:15 -0700

    added pubsub.js

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,7 +1,14 @@
 // Initiate all the components
 var Tower = require("./classes/Tower.js");
 var GameEngine = require("./classes/Game.js");
+
+// Initiate pubsub
+var PubSub = require("pubsub-js");
+
 var game;
+
+// Create extra tower cards when certain research is reached
+
 // On start, create a game element - these event listeners will be destroyed when the container is overriden
 document.getElementById("start-btn").addEventListener("click", function() {
   // Hides the modal lightbox
@@ -12,6 +19,7 @@ document.getElementById("start-btn").addEventListener("click", function() {
   game = new GameEngine;
 });
 
+// On clicking the information button, show the information panel
 document.getElementById("information-btn").addEventListener("click", function() {
   console.log("show information container here");
 });
