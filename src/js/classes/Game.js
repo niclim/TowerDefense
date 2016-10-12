a68d77dfafe80dd1843a8d43205dba2206c16c00
Nicholas Lim <nicholaslim49@gmail.com>
2016-10-11 20:47:27 -0700

    scoped out a little more

--- src/js/classes/Game.js
+++ src/js/classes/Game.js
@@ -3,21 +3,46 @@ var GameEngine = function() {
   this.level = 1;
   this.userLives = 30;
   this.activeMonsters = {}; // List of active monsters in the
-  this.towers = {};
-
+  this.towers = {}; // object of tower objects
 
   // Goes through and disables towers that can't be used yet
 }
 
+// method to check gold before place tower or upgrade
+GameEngine.prototype.checkGold = function (goldCost) {
+  if (goldCost > this.userGold) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+GameEngine.prototype.gameOver = function() {
+
+}
+
+GameEngine.prototype.gameWon = function() {
+
+}
+
 // New level method
+GameEngine.prototype.nextLevel = function() {
+
+}
+
+GameEngine.prototype.render = function () {
+  // send state to the display object to render
+};
 
 GameEngine.prototype.runCycle = function() {
   // loop through active monsters and towers and run the cycle
+  // Each runCycle method returns information for the gameEngine to
+  // process (e.g. the monster died, tower changed)
 
   // loop and check death of monsters - update accordingly
-}
 
-// method to check gold before place tower or upgrade
+  // Send information to render
+}
 
 // method to upgrade tower
 
--- src/js/interface.js
+++ src/js/interface.js
@@ -4,7 +4,9 @@ var GameEngine = require("./classes/Game.js");
 
 var game;
 // HERE IS ALL THE INTERACTION EVENT LISTENERS
-// Create extra tower cards when certain research is reached
+
+/* ============== On start click handlers ==============*/
+/* =====================================================*/
 
 // On start, create a game element - these event listeners will be destroyed when the container is overriden
 document.getElementById("start-btn").addEventListener("click", function() {
@@ -20,3 +22,22 @@ document.getElementById("start-btn").addEventListener("click", function() {
 document.getElementById("information-btn").addEventListener("click", function() {
   console.log("show information container here");
 });
+
+/* ======== Information container interactions =========*/
+/* =====================================================*/
+
+
+
+/* ================= Information Hover =================*/
+/* =====================================================*/
+// Shows information about towers or monsters if hovered over
+// or active
+
+// Get information from towerdata.js
+
+/* ========== Tower Card Click and placements ==========*/
+/* =====================================================*/
+// Click a tower card and make "active" which can be placed on UI element
+// While it is active - use #dynamic.onmousemove = function -> to Send
+// information to the canvas
+// Or maybe use on hover event to show tower??
